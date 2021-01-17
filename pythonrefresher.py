4730431fa4561cc08595c433ba439b776aaf48e7
Andrew Reiblein <andrewmreiblein013@gmail.com>
2021-01-17 23:24:16 +0200

    Add existing file

--- pythonrefresher.py
+++ pythonrefresher.py
@@ -4,5 +4,25 @@ age = '23'
 
 sentence = ("Hi my name is {} and I am {} years old".format(name,age))
 
-print(sentence)
+if int(age) > 18:
+    print(sentence)
+
+else:
+    ('You are younger than 18')   
+
+#to add comments punch the hash button 
+
+"""with multyline comments
+you can use this"""
+
+year = 1830
+
+if int(year) < 2000-2100:
+    print("Welcome to the 21st century!")
+    
+else:
+    print("You are before or after the 21st century")    
+
+
+     
 
